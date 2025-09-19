a9d9bf0fea1c7068d4fa85cabb353d3eee44240d
harryarrouida <hamzaarrouida@gmail.com>
2025-09-19 12:54:01 +0100

    fix mimeapps list, some other small changes

--- .config/hypr/scripts/toggle_music.sh
+++ .config/hypr/scripts/toggle_music.sh
@@ -3,10 +3,10 @@ PIDFILE="/tmp/waybar_music_toggle.pid"
 
 if [ -f "$PIDFILE" ]; then
     kill $(cat "$PIDFILE") 2>/dev/null
-    pkill cava
     rm "$PIDFILE"
+    pkill -RTMIN+10 waybar # Send signal to hide Cava
 else
     mpv --no-video --really-quiet --loop=inf --shuffle ~/Music/HollowKnight --playlist=<(find ~/Music/HollowKnight -type f | sort) &
     echo $! > "$PIDFILE"
-    cava &
+    pkill -RTMIN+10 waybar # Send signal to show Cava
 fi
--- .zprofile
+++ .zprofile
@@ -3,3 +3,6 @@ unset GDK_DPI_SCALE
 export XCURSOR_THEME=YourCursorThemeName
 export XCURSOR_SIZE=24
 export GTK_THEME=Materia-dark
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_QPA_PLATFORM=wayland
